165d6d54f180faab550f5358ffc0e52065f2888f
Christoph Dorn <christoph@christophdorn.com>
2015-09-16 23:30:03 -0700

    wip

--- for/firewidgets/0-window.api.js
+++ for/firewidgets/0-window.api.js
@@ -2,6 +2,7 @@
 exports.forLib = function (LIB) {
 
     const COMPONENT = require("../../../../lib/firewidgets-for-zerosystem/window.component");
+    const DATA_MAPPER  = require("../../../data/for/ccjson.record.mapper/0-window.api").forLib(LIB);
 
     var exports = {};
 
@@ -56,19 +57,155 @@ exports.forLib = function (LIB) {
         }
 
         FireWidgetsComponent.prototype.instanciateComponent = function (config) {
-            
+
             var factory = context.getComponentInstanceFactory(config.impl);
-            
+
             if (!factory) {
                 throw new Error("No component instance factory found for component implementation alias '" + config.impl + "'. Make sure the component implementation is being loaded.");
             }
+            
+            function initTemplate () {
+                return LIB.Promise.try(function () {
 
-            var component = new factory({
-                domNode: config.domNode,
-                script: context.getComponentScript(config.id)
+                    var template = new context.contexts.adapters.template.firewidgets.Template();
+
+                    template.attachDomNode(config.domNode);
+
+                    return template;
+                });
+            }
+
+            function wireComponent () {
+                var script = context.getComponentScript(config.id);
+                if (!script) return LIB.Promise.resolve({});
+                return new LIB.Promise(function (resolve, reject) {
+                    try {
+                        script({
+                            wireComponent: function (wiring) {
+
+                                var componentContext = {};
+
+                                var dataConsumer = null;
+
+                                if (typeof wiring.mapData === "function") {
+                                    // TODO: Make which adapter to use configurable when refactoring to sue ccjson
+                                    dataConsumer = new (context.contexts.adapters.data["ccjson.record.mapper"]).Consumer();
+
+                                    // TODO: Make congigurable
+                                    dataConsumer.setSourceBaseUrl("/api/page" + context.contexts.page.getPath() + "/firewidgets/" + config.id + "/pointer");
+
+                                    dataConsumer.mapData(wiring.mapData(componentContext, dataConsumer));
+                                }
+
+                                return resolve(LIB._.assign(wiring, {
+                                    dataConsumer: dataConsumer
+                                }));
+                            }
+                        });
+                    } catch (err) {
+                        console.error("Error wiring component using script:", err.stack);
+                        return reject(err);
+                    }
+                });
+            }
+
+            return LIB.Promise.all([
+                wireComponent(),
+                initTemplate()
+            ]).spread(function (wiring, template) {
+
+                var componentState = {};
+                var componentContext = {
+                    get: function (name) {
+                        return componentState[name];
+                    },
+                    set: function (name, value) {
+                        componentState[name] = value;
+                    }
+                };
+                var dataObject = {};
+
+                var fillHelpers = {};
+                var templateComponentHelpers = template.getComponentHelpers();
+                Object.keys(templateComponentHelpers).forEach(function (name) {
+                    fillHelpers[name] = function () {
+                        var args = Array.prototype.slice.call(arguments);
+                        args.unshift(componentContext);
+                        return templateComponentHelpers[name].apply(null, args);
+                    }
+                });
+
+
+                // Called ONCE
+                function markupComponent () {
+                    return LIB.Promise.try(function () {
+                        if (!wiring.markup) return;
+                        return wiring.markup(
+                            componentContext,
+                            config.domNode,
+                            dataObject
+                        );
+                    });
+                }
+
+                function ensureDataLoaded (fillComponentTrigger) {
+                    if (!wiring.dataConsumer) {
+                        return LIB.Promise.resolve();
+                    }
+                    return wiring.dataConsumer.ensureLoaded().then(function () {
+                        wiring.dataConsumer.on("changed", function () {
+                            fillComponentTrigger();
+                        });
+                    });
+                }
+
+                // Called every time data CHANGES
+                function fillComponent () {
+                    return LIB.Promise.try(function () {
+                        if (
+                            !wiring.fill ||
+                            !wiring.dataConsumer
+                        ) return;
+
+                        // Re-assign all data keys
+                        Object.keys(dataObject).forEach(function (name) {
+                            delete dataObject[name];
+                        });
+                        LIB._.assign(dataObject, wiring.dataConsumer.getData());
+
+                        return wiring.fill.call(
+                            fillHelpers,
+                            componentContext,
+                            config.domNode,
+                            dataObject
+                        );
+                    });
+                }
+
+                // First we fill the component with data
+                return ensureDataLoaded(fillComponent).then(function() {
+                    return fillComponent();
+                }).then(function () {
+                    // Then we mark up the component once
+                    return markupComponent();
+                })
+// TODO: Merge wiring with component using backbone extends
+                .then(function () {
+                    var component = new factory({
+                        domNode: config.domNode,
+                        dataConsumer: wiring.dataConsumer
+                    });
+
+
+                    component.destroy = function () {
+                        if (wiring.dataConsumer) {
+                            wiring.dataConsumer.removeAllListeners();
+                        }
+                    }
+
+                    return component;
+                });
             });
-
-            return LIB.Promise.resolve(component);
         }
 
         return new FireWidgetsComponent(context);
